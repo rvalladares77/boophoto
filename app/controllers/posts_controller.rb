5431518a7f89ac6be898d7968f67404686701340
rvalladares77 <bobyvalla@gmail.com>
2016-04-09 16:00:31 -0600

    CRUD and error message finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
 
 	def index
 		@posts = Post.all
@@ -9,14 +10,50 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		@post = Post.create(post_params)
-		redirect_to posts_path
+		if @post = Post.create(post_params)
+			flash[:success] = "Your post has been created!"
+			redirect_to posts_path
+		else
+			flash.now[:alert] = "Your new post couldn't be created! Please check the form"
+			render :new
+		end
 		
 	end
 
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			flash[:success] = "Post updated."
+			redirect_to posts_path
+		else
+			flash.now[:alert] = "Update failed. Please check the form."
+			render :edit
+		end
+	end
+
+	def destroy
+		if @post.destroy
+			flash[:success] = "Post deleted."
+			redirect_to root_path
+		else
+			flash.now[:alert] = "Ups! something went wrong. Please, Report the problem to info@boophoto.com."
+			redirect_to posts_path
+ 		end
+	end
+
 	private
+
 	def post_params
 		params.require(:post).permit(:image, :caption)
+	end
+
+	def set_post
+		@post = Post.find(params[:id])
 	end
 
 end
