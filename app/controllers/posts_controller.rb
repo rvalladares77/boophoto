d089022f9638bec2c268859958bccd3c51cabc03
rvalladares77 <bobyvalla@gmail.com>
2016-04-09 19:36:50 -0600

    view my own post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,18 @@
 class PostsController < ApplicationController
 	before_action :set_post, only: [:show, :edit, :update, :destroy]
 	before_action :authenticate_user!
+	before_action :owned_post, only: [:edit, :update, :destroy]
 
 	def index
 		@posts = Post.all
 	end
 
 	def new
-		@post = Post.new
+		@post = current_user.posts.build
 	end
 
 	def create
-		if @post = Post.create(post_params)
+		if @post = current_user.posts.build(post_params) #@Post.create(post_params)
 			flash[:success] = "Your post has been created!"
 			redirect_to posts_path
 		else
@@ -55,6 +56,13 @@ class PostsController < ApplicationController
 
 	def set_post
 		@post = Post.find(params[:id])
+	end
+
+	def owned_post
+    	unless current_user == @post.user
+    		flash[:alert] = "That post doesn't belong to you!!!!"
+    		redirect_to root_path
+    	end
 	end
 
 end
