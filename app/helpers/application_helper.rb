08f52139bca4a603c55fc36ef6889cff54b9678e
rvalladares77 <bobyvalla@gmail.com>
2016-04-10 15:40:22 -0600

    add default image and adding nice  css for the login form and registration form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
 	def alert_for(flash_type)
 		{ success: 'alert-success',
 		  error: 'alert-danger',
@@ -6,4 +7,14 @@ module ApplicationHelper
 		  notice: 'alert-info'
 		}[flash_type.to_sym] || flash_type.to_s
 	end
+
+	def form_image_select(post)
+		if post.image.exists?
+           return image_tag post.image.url(:medium), id: 'image-preview',  class: 'img-responsive'
+        else
+        	return image_tag 'placeholder', id: 'image-preview',  class: 'img-responsive'
+		end
+	end
+
+
 end
